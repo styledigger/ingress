e091d4be80614a3085535b2df4b557d671715026
Marian Kuna <mkunask@gmail.com>
2018-04-30 10:42:18 +0200

    adder rbac.yaml to create-ingress script

--- create-ingress.sh
+++ create-ingress.sh
@@ -14,12 +14,9 @@ sleep 10
 
 kubectl create sa oraclebmc-provisioner -n=$NAMESPACE
 
-kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-default-backend-deployment.yaml -n=$NAMESPACE
-
-kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-default-backend-service.yaml -n=$NAMESPACE
-
-kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-config.yaml -n=$NAMESPACE
-
-kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-ingress-controller-deployment.yaml -n=$NAMESPACE
-
-kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-ingress-controller-service.yaml -n=$NAMESPACE
+kubectl create -f https://raw.githubusercontent.com/styledigger/ingress/master/rbac.yaml -n=$NAMESPACE
+kubectl create -f https://raw.githubusercontent.com/styledigger/ingress/master/nginx-default-backend-deployment.yaml -n=$NAMESPACE
+kubectl create -f https://raw.githubusercontent.com/styledigger/ingress/master/ingress/nginx-default-backend-service.yaml -n=$NAMESPACE
+kubectl create -f https://raw.githubusercontent.com/styledigger/ingress/master/ingress/nginx-config.yaml -n=$NAMESPACE
+kubectl create -f https://raw.githubusercontent.com/styledigger/ingress/master/ingress/nginx-ingress-controller-deployment.yaml -n=$NAMESPACE
+kubectl create -f https://raw.githubusercontent.com/styledigger/ingress/master/ingress/nginx-ingress-controller-service.yaml -n=$NAMESPACE
